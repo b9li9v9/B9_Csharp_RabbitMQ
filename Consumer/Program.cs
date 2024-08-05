0dbc4c2d6d4a3398056223c8c23db4a85e551f4c
b9li9v9 <b9li9v9>
2024-08-05 18:31:36 +0800

    添加项目文件。

---
+++ Consumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+namespace Consumer { 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 如果我们想连接到另一台机器上的节点，我们只需在此处指定其主机名或 IP 地址即可。
+            var factory = new ConnectionFactory{HostName = "localhost"};
+            using (var connection = factory.CreateConnection()) 
+            {
+                using (var channel = connection.CreateModel()) 
+                {
+                }
+            
+            }
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+```
+流程
+1.下载RabbitMQ 配路径
+2.下载RabbitMQ浏览器 rabbitmq-plugins enable rabbitmq_management  
+	http://localhost:15672  账号密码默认是：guest/guest  
+	guest 用户是本地账号如果 不在同一台服务器上无法连接 需要新建一个账号并且给这个账号对应的权限
+3.NuGet RabbitMQ.Client
+4.写代码
+```
+
+
+```
+资源
+阿里云教程 https://developer.aliyun.com/article/769883
+博客园教程 https://www.cnblogs.com/xiongyang123/p/14272115.html
+RabbitMQ官网 C# https://www.rabbitmq.com/tutorials/tutorial-one-dotnet
+
+```
